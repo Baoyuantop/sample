50820967b9b9463f6e8807dc840719303d89e121
Baoyuan <1045149141@qq.com>
2018-06-04 14:43:26 +0000

    deletemailserver

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -43,8 +43,7 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
-        $this->sendEmailConfirmationTo($user);
-        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        Auth::login($user);
         return redirect('/');
 
     }
@@ -88,30 +87,9 @@ class UsersController extends Controller
         return back();
     }
 
-    protected function sendEmailConfirmationTo($user)
-    {
-        $view = 'emails.confirm';
-        $data = compact('user');
-        $to = $user->email;
-        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($to, $subject) {
-            $message->to($to)->subject($subject);
-        });
-    }
 
-    public function confirmEmail($token)
-    {
-        $user = User::where('activation_token', $token)->firstOrFail();
 
-        $user->activated = true;
-        $user->activation_token = null;
-        $user->save();
-
-        Auth::login($user);
-        session()->flash('success', '恭喜你，激活成功！');
-        return redirect()->route('users.show', [$user]);
-    }
 
     public function followings(User $user)
     {
